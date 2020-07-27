971beeed532377813cafe137a202d760388eab3c
CoverOWO <ImEasyCJ@gmail.com>
2020-07-27 12:19:45 -0700

    getting somewhere

--- how.c
+++ how.c
@@ -1,24 +1,20 @@
 # include <stdio.h> 
 # include <stdlib.h>
-# include <time.h> // including is like importing a package
-# define MAX_LIMIT 20 // variable defining
+# include <time.h> 
+# define MAX_LIMIT 20 
 
-// must put int main() {} to beable to code 
 int main() {
 
 	srand(time(0));
 
-	int chr[MAX_LIMIT]; // makes it so u can have more then 1 charater in this string
 	int numbs = rand();
 	int cumb = rand();
+	int t;
 
 	printf(" %d + %d = ", numbs, cumb);
 	
-	//fgets(chr, MAX_LIMIT, stdin);
-	
-	//printf("You entered %s", chr);
+	scanf("%d", &t);
+
 	return 0;
 
 }
-
-// gcc how.c is how to compile program O_O
