b69ff146f78dfc6dc13003241e64f82f2c6cdba4
Josh Smith <cmyuiosu@gmail.com>
2020-07-27 16:24:43 -0400

    Update how.c

--- how.c
+++ how.c
@@ -1,30 +1,27 @@
-# include <stdio.h> 
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-
 	int t;
 	int score = 0;
 
-	while(1)
-	{
+	while(1) {
 		int numbs = rand() % 10 + 1;
 		int cumb = rand() % 10 + 1;
 		int answer = numbs + cumb;
 
 		printf("%d + %d = ", numbs, cumb);
-	
+
 		scanf("%d", &t);
 
-		if (t == answer)
-		{
-			score += 1;
+		if (t == answer) {
+			score++;
 			printf("You are right! Current Streak: %d\n", score);
-		} 
-		else {
-			return printf("No the answer is %d\nTry again by opening the exe again", answer);
 		}
-		
+		else {
+			printf("No the answer is %d\nTry again by opening the exe again", answer);
+		}
 	}
 
+    return 0;
 }
